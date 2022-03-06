3a4c81ff96d95fa2e610bb5c1782b2bcaead1af1
unknown <rozfire44@gmail.com>
2022-03-06 13:48:48 +0500

    sql hw_2 style fix

--- Sql_hw/sql_hw_2.sql
+++ Sql_hw/sql_hw_2.sql
@@ -1,8 +1,8 @@
-/*
-1. Создать таблицу employees
-- id. serial,  primary key,
-- employee_name. Varchar(50), not null
- */
+
+-- 1. Создать таблицу employees
+-- id. serial,  primary key,
+-- employee_name. Varchar(50), not null
+
 
 create table employees (
 	id serial primary key,
